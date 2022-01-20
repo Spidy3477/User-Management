279e5421a449f8062f7ead0a490613c52e63493e
Spidy3477 <97495007+Spidy3477@users.noreply.github.com>
2022-01-20 23:01:32 +0530

    Delete src directory

--- src/main/java/com/example/userapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.userapi.repository;
-
-import com.example.userapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    //all crud database method
-
-}
